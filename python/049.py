014237c9d9205bec477562d1022b970ea47b376f
kitagawa-hr <kitagawa@cancerscan.jp>
2018-10-02 22:55:55 +0900

    solve 48 49

--- python/049.py
+++ python/049.py
@@ -15,18 +15,33 @@ What 12-digit number do you form by concatenating the three terms in this
 sequence?
 """
 
-from itertools import combinations, permutations
+from itertools import combinations,combinations_with_replacement, permutations
 
-from functions import is_prime
+from functions import is_prime, list_to_num
+
+
+def is_arithmetic(seq):
+    if seq[2] + seq[0] == 2 * seq[1] and seq[0] != seq[1]:
+        return True
+    return False
 
 
 def main():
-    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    combs = combinations(nums, 4)
+    ans = []
+    nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    combs = combinations_with_replacement(nums, 4)
     for comb in combs:
-        primes = [x for x in permutations(comb) if is_prime(x)]
+        primes = [
+            list_to_num(x) for x in permutations(comb)
+            if is_prime(list_to_num(x)) and list_to_num(x) > 1000
+        ]
         if len(primes) <= 2:
             continue
+        for three_primes in combinations(primes, 3):
+            if is_arithmetic(sorted(three_primes)):
+                ans.append(three_primes)
+    true_ans = [x for x in ans if 8147 not in x]
+    print(list_to_num((true_ans[0])))
     return
 
 
