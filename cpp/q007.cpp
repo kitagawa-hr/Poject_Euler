57e55ccd4b868552596b2fc5512e8773e0175f3b
kitagawa-hr <kitagawa@cancerscan.jp>
2018-10-04 18:21:54 +0900

    add cpp prime func

--- cpp/q007.cpp
+++ cpp/q007.cpp
@@ -1,31 +1,17 @@
 #include<iostream>
 #include<cmath>
 #include<string>
+#include "utils.hpp"
 
 using namespace std;
 
 
-int is_prime(unsigned int n){
-    float lim=sqrt(n);
-    if (n==2){
-        return true;
-    }
-    if (n%2==0){
-        return false;
-    }
-    for (int i=3; i<=lim; i+=2){
-        if (n%i==0){
-            return false;
-        }
-    }
-    return true;
-}
 
 int main(){
     int count=0;
     int num=2;
     while (count<10001){
-        if (is_prime(num)){
+        if (PrimeUtils::isPrime(num)){
             count+=1;
         }
         num+=1;
