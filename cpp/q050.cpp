b2f312456b02843974be93623cb07d57a3738080
kitagawa-hr <kitagawa@cancerscan.jp>
2018-11-07 17:41:42 +0900

    Add cpp/q50

--- cpp/q050.cpp
+++ cpp/q050.cpp
@@ -7,9 +7,11 @@
 
 
 using namespace std;
-
+// consective prime -> 2 + 3 + 5 + 7 + 13 + 17 = 47 など
+// 1000000以下の最長のconsective primeを求めよ
 
 int main()
 {
-    PrimeUtils::isPrime()
+    vector<int> vec;
+    
 }
