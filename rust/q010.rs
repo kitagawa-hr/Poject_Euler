fa5459850721a7ac9bf196d35190ff7862160815
kitagawa-hr <kitagawa@cancerscan.jp>
2018-12-23 19:32:16 +0900

    Add rust 009, 010 solver

---
+++ rust/q010.rs
@@ -0,0 +1,25 @@
+
+fn main(){
+    let mut ans:i64 = 2;
+    for n in 3..2000000{
+        if is_prime(n){
+            ans += n as i64;
+        }
+    }
+    println!("{}", ans);
+}
+
+fn is_prime(num:i32)->bool{
+    if num % 2 ==0{
+        return false;
+    }
+    let mut i = 3;
+    while i32::pow(i, 2) < num + 1 {
+        if num % i ==0 {
+        return false;
+        }else{
+            i += 2;
+        }
+    }
+    true
+}
